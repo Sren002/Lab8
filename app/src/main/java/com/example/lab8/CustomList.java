19fbd8eb28b9d5898dd0e73fd9115866fa052c6e
Sren002 <sethren2002@gmail.com>
2024-10-25 11:30:49 -0600

    countCitiesTest with implementation

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -77,6 +77,10 @@ public class CustomList extends ArrayAdapter<City> {
     public void deleteCity(City city){
         cities.remove(city);
     }
+    public int countCities(){
+        return cities.size();
+    }
+
 
 
 }
